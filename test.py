23b1c60d8f914580b875e37ca6b259c2c38850b8
HarshRaj29004 <142659829+HarshRaj29004@users.noreply.github.com>
2026-06-01 19:26:33 +0530

    feat: ingest-data to vectorDB

--- test.py
+++
@@ -1,4 +0,0 @@
-# from backend.app.rag.chat import search_chunks, embed_text
-
-# # embed_fn: Callable[[str], list[float]]
-# matches = search_chunks("what is engagement rate", top_k=5, embed_fn=embed_text)
